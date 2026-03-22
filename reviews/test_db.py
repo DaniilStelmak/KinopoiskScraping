c40754d7dc2dbda294b8582913cc98adeed7f9d3
DaniilStelmak <154973215+DaniilStelmak@users.noreply.github.com>
2026-03-22 22:16:21 +0300

    Delete reviews/test_db.py

--- reviews/test_db.py
+++
@@ -1,9 +0,0 @@
-from db import init_db, get_connection
-
-init_db()
-
-with get_connection() as conn:
-    with conn.cursor() as cur:
-        cur.execute("SELECT NOW() AS now_time")
-        row = cur.fetchone()
-        print("DB OK:", row)
